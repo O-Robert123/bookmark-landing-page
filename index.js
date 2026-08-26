65dc6b208839d377ad153a077cdd2d7d4d316a7d
Robert <onuoharobert8@gmail.com>
2026-08-26 15:00:02 +0100

    mobile implementation

--- index.js
+++ index.js
@@ -32,15 +32,31 @@ const ftBtns = document.querySelectorAll('.feature-btns button')
 const ftDivs = document.querySelectorAll('.sect2 .feature')
 console.log(ftBtns, ftDivs)
 ftBtns.forEach((btn, index) => {
-    btn.addEventListener('click', function(){
+    btn.addEventListener('click', function () {
         ftBtns.forEach(bt => {
             bt.classList.remove('orange')
             this.classList.add('orange')
         })
         ftDivs.forEach((ft, ftindex) => {
-            index === ftindex? ft.hidden = false: ft.hidden = true
+            index === ftindex ? ft.hidden = false : ft.hidden = true
             btn
             console.log(index, ftindex)
         })
     })
+})
+
+const menuBtn = document.querySelector('#mobile-menu')
+const menuBtnPath = document.querySelector('#mobile-menu path')
+const mobileMenu = document.querySelector('.nav-right')
+const logoCircle = document.querySelector('#logo circle')
+const logoInnerIcon = document.querySelector('#logo path:nth-child(2)')
+const logoText = document.querySelector('#logo path:nth-child(1)')
+const mobileMenuSocialIcons = document.querySelector('#nav-right-icons')
+menuBtn.addEventListener('click', function () {
+    logoCircle.classList.toggle('white')
+    logoText.classList.toggle('white')
+    logoInnerIcon.classList.toggle('black')
+    menuBtnPath.classList.toggle('white')
+    mobileMenuSocialIcons.hidden = !mobileMenuSocialIcons.hidden
+    mobileMenu.hidden = !mobileMenu.hidden
 })
